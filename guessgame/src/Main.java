9a6910297c8eede9320011a54b16decdb5e53520
Isleony <129456301+Isleony@users.noreply.github.com>
2024-05-26 16:45:24 -0300

    New

--- guessgame/src/Main.java
+++ guessgame/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         GuessGame game = new GuessGame();
@@ -5,50 +7,25 @@ public class Main {
     }
 
     static class GuessGame {
-
-        class Player {
-            int number;
-
-            void guess() {
-                number = (int) (Math.random() * 10);
-                System.out.println("O jogador deu seu palpite: " + number);
-            }
-        }
-
         void startGame() {
-            Player p1 = new Player();
-            Player p2 = new Player();
-            Player p3 = new Player();
+            Scanner scanner = new Scanner(System.in);
 
             int targetNumber = (int) (Math.random() * 10);
-            System.out.println("Pensando em um número entre 0 e 9");
+            System.out.println("Pensando em um número entre 0 e 9. Tente adivinhar!");
 
             while (true) {
-                System.out.println("O número a adivinhar é: " + targetNumber);
-                p1.guess();
-                p2.guess();
-                p3.guess();
+                System.out.print("Digite seu palpite: ");
+                int userGuess = scanner.nextInt();
 
-                int guessp1 = p1.number;
-                int guessp2 = p2.number;
-                int guessp3 = p3.number;
-
-                if (guessp1 == targetNumber || guessp2 == targetNumber || guessp3 == targetNumber) {
-                    System.out.println("Pelo menos um jogador acertou!");
-                    if (guessp1 == targetNumber) {
-                        System.out.println("Jogador 1 acertou!");
-                    }
-                    if (guessp2 == targetNumber) {
-                        System.out.println("Jogador 2 acertou!");
-                    }
-                    if (guessp3 == targetNumber) {
-                        System.out.println("Jogador 3 acertou!");
-                    }
+                if (userGuess == targetNumber) {
+                    System.out.println("Parabéns! Você acertou!");
                     break;
                 } else {
-                    System.out.println("Nenhum jogador acertou, continuando o jogo...");
+                    System.out.println("Errado. Tente novamente.");
                 }
             }
+
+            scanner.close();
         }
     }
 }
